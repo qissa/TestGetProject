f140985816bc123349df29f1134545ba320bb537
Qusay <qissa.uba@gmail.com>
2016-02-22 14:18:38 +0200

    Initial Commit

---
+++ TestGetProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGetProject
+//
+//  Created by Qusay Bany Issa on 2/22/16.
+//  Copyright © 2016 Qusay Bany Issa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGetProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestGetProject
+//
+//  Created by Qusay Bany Issa on 2/22/16.
+//  Copyright © 2016 Qusay Bany Issa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
