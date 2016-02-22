5f73d381899aadd30e9ff60d3d25207450e969f3
Qusay <qissa.uba@gmail.com>
2016-02-22 16:00:55 +0200

    NewChanges

--- TestGetProject/AppDelegate.m
+++ TestGetProject/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
+    //From App Delegate
     return YES;
 }
 
